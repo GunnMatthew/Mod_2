c8846f172b4e2d6822b575ac90317e59c24cacf8
Matthew Gunn <Gunnmatthewe@gmail.com>
2024-04-11 10:48:00 -0700

    Adding test.js content

---
+++ test.js
@@ -0,0 +1,12 @@
+/* 3 examples of basic arithmetic in JS */
+console.log(5 * 2);
+console.log(10 / 2);
+console.log(5 - 2);
+
+/* 2 examples of declaring variables in JS */
+let x = 5;
+let y = 2;
+
+/* 2 examples of using variables in arithmetic operations in JS */
+console.log(x + y);
+console.log(x * y);
